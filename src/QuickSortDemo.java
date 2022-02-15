8313fbecb5798bca22b82e3928266c3b19c4b5d9
Stephan Kochauf <kc@htl-kaindorf.at>
2022-02-15 06:24:30 +0100

    Init

---
+++ src/QuickSortDemo.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class QuickSortDemo {
+
+
+    public static void main(String[] args) {
+
+        int[] array = {9, 8, 7, 5, 4, 3, 2, 1};
+
+        sortArray(array, 0, array.length - 1);
+
+        System.out.println(Arrays.toString(array));
+    }
+
+
+    public static void sortArray(int[] array, int start, int end) {
+
+    }
+
+    /*Findet Index von Pivot-Element und sortiert*/
+    private static int partition(int[] array,int start, int end) {
+        return -1;
+    }
+}
